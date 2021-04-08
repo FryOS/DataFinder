e459dd1a597a36fb89ad716c62d7e28e639aec9e
Alexey Ospishchev <alexeyosp@gmail.com>
2021-04-08 22:49:25 +0300

    First commit

---
+++ DataFinder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
